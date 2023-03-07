17b0fe2c31e3943801b3f45ca992012cb832d7fe
laukik123 <94339169+laukik123@users.noreply.github.com>
2023-03-07 19:30:46 +0530

    uselocation hook used to show the active tab in navbar

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,11 @@
 import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/noteContext'
 
 const About = () => {
-  const a=useContext(noteContext);
-  useEffect(() => {
-    a.update();
-  }, [])
+  
   
   return (
     <div>
-      This is About {a.state.name}, studies in class {a.state.class}
+      This is About page
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,22 +3,9 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState=(props)=>{
-    const s1={
-        "name":"raj",
-        "class":"10a"
-    }
-    const [state, setstate] = useState(s1);
-    const update=()=>{
-            setTimeout(() => {
-                setstate({
-                    "name":"ajay",
-                    "class":"10c"
-                })
-            }, 1000);
-    }
 
 return(
-    <NoteContext.Provider value={{state,update}}>
+    <NoteContext.Provider value={{}}>
        { props.children}
     </NoteContext.Provider>
 )
